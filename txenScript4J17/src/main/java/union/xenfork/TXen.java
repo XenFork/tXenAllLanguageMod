d0d22f22f91a3e6b86761e498d289d223869ce6d
baka4n <qq474899581@gmail.com>
2022-11-21 14:37:38 +0800

    update

--- txenScript4J17/src/main/java/union/xenfork/TXen.java
+++ txenScript4J17/src/main/java/union/xenfork/TXen.java
@@ -1,12 +1,56 @@
 package union.xenfork;
 
-public class TXen {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class TXen {
+    public static final List<String> extension = new ArrayList<>();
+    public static void main(String[] args) {
+        //test
+        init();
+        File file = new File(System.getProperty("user.dir"), "test");
+        var a = loadFile(file);
+        for (var b : a) {
+            System.out.println(b);
+            try {
+                read(b);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    
+    public static void read(File file) throws IOException {
+        Path path = file.toPath();
+        Scanner scanner = new Scanner(path);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+        }
+    }
 
 
+    public static void init() {
+        //注入添加后缀名
+        extension.add(".xs");
+    }
+    //写入教科书式的读取文件列表
+    public static List<File> loadFile(File file) {
+        List<File> files = new ArrayList<>();
+        final var listFiles = file.listFiles();
+        final var stream = extension.stream();
+        if (listFiles != null)
+            for (var f : listFiles)
+                if (f.isDirectory())
+                    files.addAll(loadFile(f));
+                else
+                    stream.filter(e -> f.getName().contains(e)).map(e -> f).forEach(files::add);
+        return files;
+    }
 
 }
